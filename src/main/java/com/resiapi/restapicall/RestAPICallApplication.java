eed886917d3d85fd245807e0f7984ce7a1f75f7e
viveksahu06 <viveksahu1006@gmail.com>
2025-02-21 20:55:09 +0530

    [Vivek] Add. Dependency Added

---
+++ src/main/java/com/resiapi/restapicall/RestAPICallApplication.java
@@ -0,0 +1,13 @@
+package com.resiapi.restapicall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAPICallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAPICallApplication.class, args);
+	}
+
+}
